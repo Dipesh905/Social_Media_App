e2d31100e356e9feffdf47da04adc60810b4dbef
Dipesh Ghi <ghimiredipesh5@gmail.com>
2023-10-03 09:14:17 +0545

    social media app first push

---
+++ lib/view/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('SocialMediaApp'),
+      ),
+    );
+  }
+}
